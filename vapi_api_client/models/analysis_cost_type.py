1a9522c989a2e4e862ea4daa154abf828a8fb21f
Madhav Patel <pmadhavvv@gmail.com>
2025-05-11 21:35:30 +0530

    feat: updated analysis cost type enum

--- vapi_api_client/models/analysis_cost_type.py
+++ vapi_api_client/models/analysis_cost_type.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class AnalysisCostType(str, Enum):
     ANALYSIS = "analysis"
+    KNOWLEDGE_BASE = "knowledge-base"
 
     def __str__(self) -> str:
         return str(self.value)
